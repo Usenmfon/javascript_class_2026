f8fc1b054202f6e8a78635908156654e799c95f4
Usenmfon <usenmfonuko@gmail.com>
2026-06-04 14:41:59 +0100

    update: api, get, post

--- API 3/app.js
+++ API 3/app.js
@@ -30,6 +30,41 @@ async function createTodo(payload)
         body: JSON.stringify(payload)
     })
 
-    const data = await response.json()
-    console.log(data)
+    // const data = await response.json()
+    
+    window.location.reload()
 }
+
+
+async function getTodos()
+{
+    const response = await fetch("https://js-todo-api-express.vercel.app/api/todos")
+
+    const todos = await response.json()
+
+    displayTodos(todos.data)
+}
+
+getTodos()
+
+let body = document.body
+let section = document.createElement('section')
+
+function displayTodos(todos)
+{
+    todos.forEach((todo) => {
+        let div = document.createElement('div')
+        let p = document.createElement('p')
+        let desc = document.createElement('p')
+        let createdAt = document.createElement('p')
+
+        p.textContent = `Title: ${todo.title}`
+        desc.textContent = `Desc: ${todo.description}`
+        createdAt.textContent = `Created At: ${todo.createdAt}`
+
+        div.append(p, desc, createdAt)
+        section.appendChild(div)
+    })
+}
+
+body.appendChild(section)
